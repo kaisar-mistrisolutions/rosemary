cc5b33c2c2caebac22f3c4cbcf06be32992713ed
KaisarJamil <kaisar.mistrisolutions@gmail.com>
2021-06-07 14:59:36 +0600

    product create

--- resources/views/livewire/cat-sub.blade.php
+++ resources/views/livewire/cat-sub.blade.php
@@ -1,6 +1,6 @@
 <div>
      <select id="category" name="category" wire:model="selectedCategory" autocomplete="category" class="max-w-lg block focus:ring-indigo-500 focus:border-indigo-500  shadow-sm sm:max-w-lg sm:text-sm border-gray-300 rounded-md">
-            <option value="" hidden >
+            <option hidden>
                 Select Category
             </option>
             @foreach($categories as $category)
@@ -12,7 +12,7 @@
     @if (!is_null($sub_categories))
     <div class="mt-8">
     <select id="sub-category" name="sub_category" wire:model="selectedSubCategory" autocomplete="sub-category" class="max-w-lg block focus:ring-indigo-500 focus:border-indigo-500  shadow-sm sm:max-w-lg sm:text-sm border-gray-300 rounded-md">
-            <option selected="" disabled="" hidden="">
+            <option hidden>
                 Select Sub Category
             </option>
             @foreach($sub_categories as $sub_category)
